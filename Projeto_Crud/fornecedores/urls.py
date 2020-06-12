2bd477a9172f230730135a8cc1af44cf1a3367bf
Alan Faraj <alanfaraj@outlook.com>
2020-06-12 15:35:34 -0300

    excluindo

--- Projeto_Crud/fornecedores/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home1, fornecedor
-
-
-urlpatterns = [
-    path('', home1 ),
-    path('fornecedor/<int:codigo>/', fornecedor),
-
-]
--- Projeto_Crud/produtos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, produto
-
-urlpatterns = [
-    path('', home),
-    path('produto/<int:codigo>/', produto),
-]
-
-# request > urls > model > response ( template )
