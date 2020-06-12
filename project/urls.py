17b4d9e95983a464c5816008f88a46885e329ba3
Alan Faraj <alanfaraj@outlook.com>
2020-06-12 16:36:33 -0300

    adicionando o projeto

---
+++ project/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from core.views import index
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name="index"),
+    path('', include('products.urls'))
+
+]
